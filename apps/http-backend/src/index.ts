c7e2bfa0d4bc8b2a7f1c3b0f47ddbb0ba251433d
Diotoara <aapka.5ryan@gmail.com>
2026-01-06 02:54:13 +0530

    jwts and auth

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,5 +1,32 @@
 import express from "express";
+import jwt from "jsonwebtoken"
+import middleware from "./middleware";
 
 const app = express();
+
+app.use(express.json())
+
+app.post('/signup', (req,res)=>{
+    const {username, password} = req.body();
+
+})
+
+app.post('/signin', (req,res)=>{
+    const {username, password} = req.body();
+
+    const userId=1;
+    const token = jwt.sign({userId}, "jwtsecret")
+
+})
+
+app.post("/room", middleware , (req,res)=>{
+
+    //db call
+
+    res.json({
+        roomid : 12
+    })
+
+})
 
 app.listen(3000)
--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,9 +1,24 @@
 import { WebSocketServer } from 'ws';
-
+import jwt from "jsonwebtoken"
 const wss = new WebSocketServer({ port: 8080 });
 
-wss.on('connection', function connection(ws) {
+interface TOKENtype {
+  userId : string
+}
 
+wss.on('connection', function connection(ws, request) {
+  const url = request.url;
+  if(!url){
+    return
+  }
+  const AllQueryParam = new URLSearchParams(url.split('?')[1]);
+  const token = AllQueryParam.get('token') || "";
+  const decoded = jwt.verify(token, "jwtsecret") as TOKENtype
+
+  if(!decoded || !decoded.userId){
+    ws.close()
+    return
+  }
   ws.on('message', function message(data) {
     ws.send('pong');
   });
